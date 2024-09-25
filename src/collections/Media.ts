e0c36d9ad85230354b1f5502a9db5575fa3c2e7d
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-09-25 03:04:26 -0400

    Removed extraneous payload stuff.

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -8,20 +8,11 @@ import {
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-import { anyone } from '../access/anyone'
-import { authenticated } from '../access/authenticated'
-
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export const Media: CollectionConfig = {
   slug: 'media',
-  access: {
-    create: authenticated,
-    delete: authenticated,
-    read: anyone,
-    update: authenticated,
-  },
   fields: [
     {
       name: 'alt',
--- src/collections/Users/index.ts
+++ src/collections/Users/index.ts
@@ -1,16 +1,7 @@
 import type { CollectionConfig } from 'payload'
 
-import { authenticated } from '../../access/authenticated'
-
 const Users: CollectionConfig = {
   slug: 'users',
-  access: {
-    admin: authenticated,
-    create: authenticated,
-    delete: authenticated,
-    read: authenticated,
-    update: authenticated,
-  },
   admin: {
     defaultColumns: ['name', 'email'],
     useAsTitle: 'name',
