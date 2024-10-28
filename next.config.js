acfc924603f574d5efe38c523712a530b5e29cd1
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-10-28 05:17:05 -0400

    And trying local patterns.

--- next.config.js
+++ next.config.js
@@ -3,23 +3,23 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // localPatterns: [
-    //   {
-    //     pathname: '/api/media/**',
-    //   },
-    // ],
-    remotePatterns: [
+    localPatterns: [
       {
-        hostname: process.env.VERCEL_PROJECT_PRODUCTION_URL ?? '',
-        protocol: 'https',
-        pathname: '/api/media/**',
-      },
-      {
-        hostname: process.env.VERCEL_URL ?? '',
-        protocol: 'https',
         pathname: '/api/media/**',
       },
     ],
+    // remotePatterns: [
+    //   {
+    //     hostname: process.env.VERCEL_PROJECT_PRODUCTION_URL ?? '',
+    //     protocol: 'https',
+    //     pathname: '/api/media/**',
+    //   },
+    //   {
+    //     hostname: process.env.VERCEL_URL ?? '',
+    //     protocol: 'https',
+    //     pathname: '/api/media/**',
+    //   },
+    // ],
   },
   reactStrictMode: true,
 }
