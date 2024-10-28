c2fbcb5a201229bc6aec1ae446fc55ba73a06e92
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-10-28 04:55:01 -0400

    Again.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 const NEXT_PUBLIC_SERVER_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+    ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
     : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-      ? process.env.NEXT_PUBLIC_VERCEL_URL
+      ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
       : 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
