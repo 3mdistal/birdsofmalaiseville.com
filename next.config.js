869ca4fe13a7054254627caa08c0054e51da07f5
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-10-28 05:16:50 -0400

    Trying with pathname.

--- next.config.js
+++ next.config.js
@@ -3,14 +3,21 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // localPatterns: [
+    //   {
+    //     pathname: '/api/media/**',
+    //   },
+    // ],
     remotePatterns: [
       {
         hostname: process.env.VERCEL_PROJECT_PRODUCTION_URL ?? '',
         protocol: 'https',
+        pathname: '/api/media/**',
       },
       {
         hostname: process.env.VERCEL_URL ?? '',
         protocol: 'https',
+        pathname: '/api/media/**',
       },
     ],
   },
