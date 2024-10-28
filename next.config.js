fd7784b9f0d2c4c2b72ef41a6f63210f0a6cabd0
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-10-28 05:10:43 -0400

    Trying NEXT PUBLIC.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
-// const NEXT_PUBLIC_SERVER_URL =
-//   process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-//     ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-//     : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-//       ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
-//       : 'http://localhost:3000'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: '**',
+        hostname: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
+        protocol: 'https',
+      },
+      {
+        hostname: process.env.NEXT_PUBLIC_VERCEL_URL,
+        protocol: 'https',
       },
     ],
   },
