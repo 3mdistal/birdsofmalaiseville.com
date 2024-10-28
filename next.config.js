fbdb4fa0552f7e69cd9b71a71f3141826308373f
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-10-28 04:52:08 -0400

    Images should work now.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,26 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+    : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+      ? process.env.NEXT_PUBLIC_VERCEL_URL
+      : 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-  //       const url = new URL(item)
+  images: {
+    remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
+        const url = new URL(item)
 
-  //       return {
-  //         hostname: url.hostname,
-  //         protocol: url.protocol.replace(':', ''),
-  //       }
-  //     }),
-  //   ],
-  // },
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', ''),
+        }
+      }),
+    ],
+  },
   reactStrictMode: true,
 }
 
