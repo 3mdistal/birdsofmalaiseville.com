7e59d4d464c2f699cf3d0bfb7fee393897027200
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-11-04 00:48:13 -0500

    Trying solution for images from Discord. (Rewrites)

--- next.config.js
+++ next.config.js
@@ -1,26 +1,22 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
-const serverUrl =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-      ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
-      : 'http://localhost:3000'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[serverUrl].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-          pathname: '/api/media/**',
-        }
-      }),
+      {
+        protocol: 'https',
+        hostname: '**.vercel-storage.com',
+      },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/media/file/:filename*',
+        destination: 'https://1des1qmcmnaqih0h.public.blob.vercel-storage.com/:filename*',
+      },
+    ]
   },
   reactStrictMode: true,
 }
