85434978e6314a4b5213146a3413c1b01c2425e9
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-12-03 03:02:04 -0500

    We have flight.

--- src/components/homepage/flock.tsx
+++ src/components/homepage/flock.tsx
@@ -1,13 +1,45 @@
+'use client'
+
 import styles from './flock.module.css'
 import Image from 'next/image'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
 
 export default function Flock() {
+  const containerRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end start'],
+  })
+
+  // Create different speeds for each bird
+  const y = useTransform(scrollYProgress, [0, 1], [0, -30000])
+
+  // Fade out as they move up
+  const opacity = useTransform(scrollYProgress, [0, 0.8], [1, 0.2])
+
   return (
-    <div className={styles.flock}>
+    <div ref={containerRef} className={styles.flock}>
       <div className={styles.extraDarkDiv}></div>
-      <div className={styles.flockContent}>
-        {/* <Image src="/flock.svg" alt="Flock" width={100} height={100} /> */}
-      </div>
+      <motion.div className={styles.flockContent} style={{ opacity, y }}>
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+        <Image src="/flock.svg" alt="Flock" width={500} height={500} priority />
+      </motion.div>
     </div>
   )
 }
